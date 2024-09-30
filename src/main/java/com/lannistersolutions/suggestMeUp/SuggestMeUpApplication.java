4810183a8e11ac1ead1904a8cd0b58427123a6a8
Irfan Ahamed <janufairfan@pop-os.lan>
2024-09-30 20:29:16 +0530

    initial skeleton

---
+++ src/main/java/com/lannistersolutions/suggestMeUp/SuggestMeUpApplication.java
@@ -0,0 +1,13 @@
+package com.suggestmeup.suggestMeUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestMeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestMeUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suggestmeup/suggestMeUp/SuggestMeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suggestmeup.suggestMeUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestMeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
