a2c2a13584897456d4dd1e306f6bca20b4a5e81f
Irfan ahamed <irfansulaiha25@gmail.com>
2024-10-05 14:04:18 +0530

    implementing homepage

---
+++ src/main/js/src/components/HomePage.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import { Box, Button, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
+
+const HomePage = (props: any) => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    console.log("Here");
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', width: '400px' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const { window } = props;
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{color:'lavender'}}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <IconButton
+            size="large"
+            edge="start"
+            onClick={handleDrawerToggle}
+            color="inherit"
+            aria-label="menu"
+            sx={{ position: 'absolute', left: 16 }} // Positioning the menu icon to the left
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ flexGrow: 1, textAlign: 'center' ,color:'black'}}>
+            SuggestMeUp!
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+};
+
+export default HomePage;
